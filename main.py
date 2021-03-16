3dd31ec35dbd9a07f179b941bbbd4bb8802a46fa
Donats Cirkins <pirateworker228@gmail.com>
2021-03-16 14:50:43 +0200

    First working drones. Go beyond left border. A bit same move

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import Player
+import enemies
+import random
 
 #prevent from crash
-
 try:
     import simplegui
 except ImportError:
@@ -22,6 +23,8 @@ class Clock:
         if self.tick == frame_duration:
             return True
 
+#used for drones
+counter = 0
 
 class Menu:
     start_btn = None
@@ -38,7 +41,6 @@ class Menu:
         self.height = height
 
     def update(self, canvas):
-
         button_dimensions = (self.start_btn.get_width(), self.start_btn.get_height())
 
         canvas.draw_image(self.start_btn, (button_dimensions[0] / 2, button_dimensions[1] / 2), button_dimensions,
@@ -99,18 +101,38 @@ def open_menu():
     frame.set_keydown_handler(menu.keyDown)
     entities.append(menu)
 
+#enemy drone. Randoms location and sets it there
+def draw_drone():
+    width = random.randint(80, WIDTH-80)
+    height = random.randint(5, HEIGHT // 2.5)
+    drone = enemies.enemyDrone("sprite_assets/helicopter_assets/HelicopterSS.png", 4, 8, Clock.frame_duration,
+                           (width, height), (320, 83.2), 32, True, 1)
+    return drone
+
 
 def start_game():
     player = Player.Player("sprite_assets/player_sprite/DroneSSTransparent.png", 4, 8, Clock.frame_duration,
                            (WIDTH / 2, HEIGHT / 2), (320, 83.2), 32, True, 1)
 
     entities.pop(0)
+    #draws the drone and adds it to the entities
+    drone = draw_drone()
+    entities.append(drone)
     entities.append(player)
     frame.set_keydown_handler(player.keyDown)
     frame.set_keyup_handler(player.keyUp)
 
 
 def draw_entities(canvas):
+    #after 300 miliseconds, new helicopter is created and counter resets
+    global counter
+    counter += 1
+    if counter == 300:
+        drone = draw_drone()
+        entities.append(drone)
+        counter = 0
+
+
     if isinstance(entities[0], Player.Player) and entities[0].get_exit_request():
         open_menu()
 
